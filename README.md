42f9754c5b83661150e5beb4f774920ed2863b7d
Anthony <anthonygst26@gmail.com>
2018-12-07 23:53:35 +0100

    Update #27 Trying to setup webpack
    TypeScript react starter doesn't seem to work.
    Multiple tests from scratch. Not successful for now.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Readme
+# Goal
+The goal of this project is to create a simultaneous, multiplayer game, based on the game scenario of the famous Bomberman.
---
+++ front-end/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import AppComponent from './app/app';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, Welcome to the first page</h1>
+    <AppComponent/>
+  </div>,
+  document.getElementById("root")
+);
