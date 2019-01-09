ab70173ba4eefa1017c55b1f510c2a1f75d5cf8d
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2019-01-09 19:02:34 +0100

    Update #10 Menu component + 'Material-icons' added

--- front-end/src/app/components/menu/Menu.tsx
+++ front-end/src/app/components/menu/Menu.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
 import 'material-design-icons/iconfont/material-icons.css';
 
 import './Menu.scss';
@@ -11,35 +11,25 @@ export default class Menu extends React.Component {
       <div className='menu'>
           <h2>Menu</h2>
           <div className='menu_button'>
-            <Button className='play' bsSize='large' bsStyle='primary' block={true}>
-              <div className='row'>
+            <Button className='purple-button row' variant='contained'>
                 <i className='material-icons md-96 col-3'>play_circle_filled</i>
-                <p className='col-6'>Play</p>
-              </div>
+                <p className='col-6 mr-3 my-auto'>Play</p>
             </Button>
-            <Button className='button' bsSize='large' block={true}>
-              <div className='row'>
+            <Button className='button row' variant='contained'>
                   <i className='material-icons md-48 col-3'>map</i>
-                  <p className='col-9'>Level Editor</p>
-              </div>
+                  <p className='col-9 my-auto'>Level Editor</p>
             </Button>
-            <Button className='button' bsSize='large' block={true} disabled={true}>
-              <div className='row'>
+            <Button className='button row' variant='contained' disabled={true}>
                   <i className='material-icons md-48 col-3'>shopping_basket</i>
-                  <p className='col-9'>Shop</p>
-              </div>
+                  <p className='col-9 my-auto'>Shop</p>
             </Button>
-            <Button className='button' bsSize='large' block={true}>
-              <div className='row'>
+            <Button className='button row' variant='contained' >
                   <i className='material-icons md-48 col-3'>show_chart</i>
-                  <p className='col-9'>Statistics</p>
-              </div>
+                  <p className='col-9 my-auto'>Statistics</p>
             </Button>
-            <Button className='button' bsSize='large' block={true}>
-              <div className='row'>
+            <Button className='button row' variant='contained'>
                   <i className='material-icons md-48 col-3'>help_outline</i>
-                  <p className='col-9'>Help</p>
-              </div>
+                  <p className='col-9 my-auto'>Help</p>
             </Button>
         </div>
       </div>
