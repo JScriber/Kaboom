4583dbdd9e25fd26a0c337d11cba6958499fa83d
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2019-01-09 18:17:05 +0100

    Update #10 Menu component + 'Material-icons' added

--- front-end/src/app/components/menu/Menu.tsx
+++ front-end/src/app/components/menu/Menu.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
+import 'material-design-icons/iconfont/material-icons.css';
 
 import './Menu.scss';
 
@@ -11,19 +12,34 @@ export default class Menu extends React.Component {
           <h2>Menu</h2>
           <div className='menu_button'>
             <Button className='play' bsSize='large' bsStyle='primary' block={true}>
-                Play
+              <div className='row'>
+                <i className='material-icons md-96 col-3'>play_circle_filled</i>
+                <p className='col-6'>Play</p>
+              </div>
             </Button>
             <Button className='button' bsSize='large' block={true}>
-                Level Editor
+              <div className='row'>
+                  <i className='material-icons md-48 col-3'>map</i>
+                  <p className='col-9'>Level Editor</p>
+              </div>
             </Button>
             <Button className='button' bsSize='large' block={true} disabled={true}>
-                Shop
+              <div className='row'>
+                  <i className='material-icons md-48 col-3'>shopping_basket</i>
+                  <p className='col-9'>Shop</p>
+              </div>
             </Button>
             <Button className='button' bsSize='large' block={true}>
-                Statistics
+              <div className='row'>
+                  <i className='material-icons md-48 col-3'>show_chart</i>
+                  <p className='col-9'>Statistics</p>
+              </div>
             </Button>
             <Button className='button' bsSize='large' block={true}>
-                Help
+              <div className='row'>
+                  <i className='material-icons md-48 col-3'>help_outline</i>
+                  <p className='col-9'>Help</p>
+              </div>
             </Button>
         </div>
       </div>
