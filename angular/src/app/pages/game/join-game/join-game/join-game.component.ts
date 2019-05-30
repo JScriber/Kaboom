12f8e11b80df0cc4484e9fe7a096c78dbfe0fc4d
Anthony <anthonygst26@gmail.com>
2019-05-30 14:11:55 +0200

    Close #8 Functional join server view

--- angular/src/app/pages/game/join-game/join-game/join-game.component.ts
+++ angular/src/app/pages/game/join-game/join-game/join-game.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy } from '@angular/core';
+import { Observable, Subject, interval } from 'rxjs';
+import { switchMap, takeUntil, startWith } from 'rxjs/operators';
+
+import { Preview } from '../models/preview.model';
+import { JoinGameApiService } from '../services/api/join-game-api.service';
+
+/** Interval when the data is refreshed (in milliseconds). */
+const REFRESH_INTERVAL = 2000;
 
 @Component({
   selector: 'app-join-game',
   templateUrl: './join-game.component.html',
   styleUrls: ['./join-game.component.scss']
 })
-export class JoinGameComponent {
+export class JoinGameComponent implements OnDestroy {
 
+  /** Handles unsubscriptions. */
+  subscriptionHandler$ = new Subject();
+
+  /** All the games available. */
+  previews$: Observable<Preview[]> = interval(REFRESH_INTERVAL).pipe(
+    startWith(0),
+    takeUntil(this.subscriptionHandler$),
+    switchMap(() => this.webService.getPreviews())
+  );
+
+  constructor(private readonly webService: JoinGameApiService) {}
+
+  /**
+   * Called when the user clicks on a preview.
+   * @param {number} id
+   */
+  access(id: number) {
+    console.log(id);
+  }
+
+  ngOnDestroy() {
+    this.subscriptionHandler$.next();
+  }
 }
